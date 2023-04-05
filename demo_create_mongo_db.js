3c8ce182f41758aa78fd31d54d0b98b9bb2b74a8
Don Tran <don.trand@gmail.com>
2023-04-04 22:39:24 -0700

    uh rip

--- demo_create_mongo_db.js
+++
@@ -1,9 +0,0 @@
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb://dontrand:123@test.ifummiy.mongodb.net/test";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
