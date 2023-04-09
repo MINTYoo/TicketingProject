b4cd86eab2612eb1eb58765da31f5e14f46ce4a5
Don Tran <don.trand@gmail.com>
2023-04-08 22:11:07 -0700

    UPdated redirect

--- add_ticket_express.js
+++ add_ticket_express.js
@@ -3,7 +3,7 @@ var path = require('path');
 var bodyParser = require('body-parser');
 const { MongoClient } = require("mongodb");
 
-const uri = "mongodb+srv://user:pass@avengerscluster.2s0a1da.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://USER:PASS@avengerscluster.2s0a1da.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 const app = express()
 
@@ -14,16 +14,22 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/QuestionForm.html')
   })
+  
+app.use(express.static(__dirname));
 
 app.post('/sendticket', function (req, res) {
-        let username = req.body.user_input;
+        let output = req.body.user_input;
         var data = {
-            "user_input": username
+            "user_input": output,
+            "priority": 1,
+            "color": 3
         }
         const database = client.db('TicketDataCollection');
         const TicketItself = database.collection('TicketData');
-        TicketItself.insertOne(data);  
-        res.send('Data received:\n' + JSON.stringify(req.body));
+        TicketItself.insertOne(data);
+        res.redirect('QuestionForm.html');
+        //res.send('Data received:\n' + JSON.stringify(req.body));
+        
 });
 
 app.listen(3000, () => {
